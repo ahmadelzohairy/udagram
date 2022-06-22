7f55d823a84725156fd2dda221e91ff412d618af
ahmadelzohairy <ahmadelzohairy93@gmail.com>
2022-06-23 01:05:05 +0200

    fix : script

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,6 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize(
-  "postgres://postgres:database-2.cgtr4v3xqnma.us-east-1.rds.amazonaws.com:5432/postgres"
-);
+export const sequelize = new Sequelize({
+  username: 'postgres',
+  password: 'qsmn123QSMN',
+  database: 'postgres',
+  host: 'database-2.cgtr4v3xqnma.us-east-1.rds.amazonaws.com',
+  port: 5432,
+
+  dialect: "postgres",
+  storage: ":memory:",
+});
