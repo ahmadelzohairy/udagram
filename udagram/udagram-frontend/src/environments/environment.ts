c400ad3435a9ca40beacbc111969af5485bcd962
ahmadelzohairy <ahmadelzohairy93@gmail.com>
2022-06-27 00:53:06 +0200

    final

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'Udagramapi-env-1.eba-pthiyqhu.us-east-1.elasticbeanstalk.com'
+  apiHost: 'Udagramapi-env-1.eba-pthiyqhu.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
