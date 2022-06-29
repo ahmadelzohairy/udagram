d0fc2be980c69ccf0eb759ee09acf7617bca171b
ahmadelzohairy <ahmadelzohairy93@gmail.com>
2022-06-29 17:55:33 +0200

    f1 1.5

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  username: "postgres",
-  password: "postgres",
-  database: "postgres",
-  host: "database-1.cwv5p0wivsj4.us-east-1.rds.amazonaws.com",
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host, //"database-1.cwv5p0wivsj4.us-east-1.rds.amazonaws.com",
 
   dialect: "postgres",
   storage: ":memory:",
--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,4 @@
+
+eb setenv POSTGRES_USERNAME=POSTGRES_USERNAME POSTGRES_PASSWORD=POSTGRES_PASSWORD POSTGRES_DB=POSTGRES_DB DB_PORT=DB_PORT POSTGRES_HOST=POSTGRES_HOST AWS_REGION=AWS_DEFAULT_REGION AWS_BUCKET=AWS_BUCKET URL=SERVER_URL JWT_SECRET=JWT_SECRET
 aws s3 cp --recursive --acl public-read ./www s3://udagram493/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram493/
